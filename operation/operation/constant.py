c8b054ff9972d085f65a9fb701c6af6ea94a0771
liubo <liubo@intraos.nsfocus.com>
2019-12-21 18:15:54 +0800

    配置文件，迁移文件提交

---
+++ operation/operation/constant.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+import os
+
+# 配置文件类型
+JSON = "F:\liubo\liubo\local_git\django\operation\conf" + os.sep + "data.json"
+YAML = "F:\liubo\liubo\local_git\django\operation\conf" + os.sep + "accessor.yaml"
+CONF = "F:\liubo\liubo\local_git\django\operation\conf" + os.sep + "config.conf"
+XML = "F:\liubo\liubo\local_git\django\operation\conf" + os.sep + "conf.xml"
